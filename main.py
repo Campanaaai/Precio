c4a7bf36c245c9037827afd72ac98cd5ae7711a2
Isaias Ortiz <>
2022-09-20 17:16:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Isaias Ortiz 4°D
+nombre = input("nombre del producto: ")
+precio = int(input("Precio neto del producto: "))
+cantidad = int(input("Cantidad: "))
+subtotal = (precio*cantidad)
+iva= (precio*19/100)
+print("Nombre del producto: ",nombre)
+print("Cantidad:", cantidad)
+print("Subtotal:", subtotal)
+print("iva:",iva)
+print("Total:" , subtotal+iva)
+
